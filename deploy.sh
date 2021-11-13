63db48b9bc7cfded9229d7f21dccaa32a27b67e1
davay42 <fond@fondfrukt.ru>
2021-11-13 15:00:06 +0300

    dep

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:davay42/axioma.center.git head:gh-pages
+git push -f git@github.com:DeFUCC/defucc.me.git head:gh-pages
 
 cd -
