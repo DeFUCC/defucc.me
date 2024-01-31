ed933c9aa35c0c1da2f82c82fd1b4e5a70e5b534
davay <davay@defucc.me>
2024-01-31 15:07:11 +0700

    deploy

--- assets/chunks/theme.1898cc3a.js
+++
@@ -1,1 +0,0 @@
-import{r as _,i as e}from"./framework.a5eec347.js";const n=_(e?location.hash:"");e&&window.addEventListener("hashchange",()=>{n.value=location.hash});_();
---
+++ assets/chunks/theme.24d680b7.js
@@ -0,0 +1,1 @@
+import{r as _,i as e}from"./framework.3782659e.js";const n=_(e?location.hash:"");e&&window.addEventListener("hashchange",()=>{n.value=location.hash});_();
